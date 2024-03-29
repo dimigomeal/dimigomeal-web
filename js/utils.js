7daaaf6cac42970ccf157946a9542ccbfa1a4ad6
noViceMin <admin@isamin.kr>
2024-03-29 09:26:11 +0900

    feat: moment.js & clean code

--- js/utils.js
+++ js/utils.js
@@ -1,146 +1,149 @@
-let nowDate = new Date();
-
-const getSec = (hour, minute, second) => {
-  return hour * 3600 + minute * 60 + second;
-}
-
-const setDateDisplay = () => {
-  $('#control #date').text(`${nowDate.getMonth() + 1}월 ${nowDate.getDate()}일 ${['일', '월', '화', '수', '목', '금', '토'][nowDate.getDay()]}요일`);
-};
+let date;
 
 const now = () => {
-  const nowTime = new Date();
-  const nowSec = getSec(nowTime.getHours(), nowTime.getMinutes(), nowTime.getSeconds());
+  const time = date.format("HH:mm:ss");
 
-  if(nowSec < getSec(10, 0, 0)) {
-    return {
-      code: 0,
-      text: 'breakfast'
-    };
-  } else if(nowSec < getSec(15, 0, 0)) {
-    return {
-      code: 1,
-      text: 'lunch'
-    };
+  if (time < "08:00:00") {
+    return 0;
+  } else if (time < "14:00:00") {
+    return 1;
   } else {
-    return {
-      code: 2,
-      text: 'dinner'
-    };
+    return 2;
   }
 };
 
-let isFirst = true;
 const getMeal = (date) => {
   $(`#app`).append(`<div id="loading" class="lds-dual-ring"></div>`);
   $.ajax({
     url: `https://api.디미고급식.com/${date}`,
-    type: 'GET',
+    type: "GET",
     success: (data) => {
-      $('#loading').remove();
       $(`#list #item #menu #dish`).remove();
-      for(const [key, value] of Object.entries(data)) {
-        if(['breakfast', 'lunch', 'dinner'].includes(key)) {
-          const menus = value.split(/\/(?![^()]*\))/)
-          for(const menu of menus) {
-            $(`#list #item.${key} #menu`).append(`<div id="dish"><p id="indicator">-</p><p id="text">${menu}</p></div>`);
-          }
-        }
-      }
+      for (const [key, value] of Object.entries(data)) {
+        if (!["breakfast", "lunch", "dinner"].includes(key)) continue;
 
-      if(isFirst) {
-        isFirst = false;
-        $('#scroll').scrollLeft($('#list #item.breakfast').width() * now().code);
+        const menus = value.split(/\/(?![^()]*\))/);
+        if (menus.length == 1 && menus[0] == "") {
+          $(`#list #item.${key} #menu`).append(
+            `<div id="dish">데이터가 없습니다</div>`
+          );
+          continue;
+        }
+
+        for (const menu of menus) {
+          $(`#list #item.${key} #menu`).append(
+            `<div id="dish"><p id="indicator">-</p><p id="text">${menu}</p></div>`
+          );
+        }
       }
     },
     error: () => {
-      $('#loading').remove();
       $(`#list #item #menu #dish`).remove();
       $(`#list #item #menu`).append(`<div id="dish">데이터가 없습니다</div>`);
-    }
+    },
+    complete: () => {
+      $("#loading").remove();
+    },
   });
-}
-
-const init = () => {
-  setDateDisplay();
-  $('#scroll').scrollLeft($('#list #item.breakfast').width() * now().code);
-  let controller = new ScrollMagic.Controller({
-    container: '#scroll',
-    vertical: false,
-    globalSceneOptions: {
-      duration: "100%"
-    }
-  });
-
-  let background1 = TweenMax.fromTo("#background.breakfast", 1, {
-    opacity: 1
-  }, {
-    opacity: 0
-  });
-  let background2 = TweenMax.fromTo("#background.lunch", 1, {
-    opacity: 1
-  }, {
-    opacity: 0
-  });
-
-  let scene1 = new ScrollMagic.Scene({
-    triggerElement: "#item.breakfast",
-    triggerHook: 0
-  })
-  .setTween(background1)
-  .addTo(controller);
-  let scene2 = new ScrollMagic.Scene({
-    triggerElement: "#item.lunch",
-    triggerHook: 0
-  })
-  .setTween(background2)
-  .addTo(controller);
-}; init();
-
-$(document).ready(() => {
-  update();
-});
-
-const dateString = (date) => {
-  const offset = new Date().getTimezoneOffset() * 60000;
-  const now = new Date(new Date(date) - offset);
-  return now.toISOString().slice(0, 10)
-}
-
-const update = () => {
-  setDateDisplay();
-  getMeal(dateString(nowDate));
 };
 
-const next = () => {
-  nowDate.setDate(nowDate.getDate() + 1);
-  update();
-}; $("#control #next").click(next);
+const control = (type) => {
+  switch (type) {
+    case "today":
+      date = moment();
+      break;
+    case "yesterday":
+      date.subtract(1, "day");
+      break;
+    case "tomorrow":
+      date.add(1, "day");
+      break;
+    default:
+      date = moment(type);
+      break;
+  }
 
-const pre = () => {
-  nowDate.setDate(nowDate.getDate() - 1);
-  update();
-}; $("#control #pre").click(pre);
+  $("#control #date").text(date.format("M월 D일 ddd요일"));
+  getMeal(date.format("YYYY-MM-DD"));
+};
 
-const today = () => {
-  nowDate = new Date();
-  update();
-}; $("#control #date").click(today);
+const init = () => {
+  control();
 
-$(document).keydown(function(e){
-  if(e.which >= 37 && e.which <= 40) {
+  let controller = new ScrollMagic.Controller({
+    container: "#scroll",
+    vertical: false,
+    globalSceneOptions: {
+      duration: "100%",
+    },
+  });
+
+  new ScrollMagic.Scene({
+    triggerElement: "#item.breakfast",
+    triggerHook: 0,
+  })
+    .setTween(
+      TweenMax.fromTo(
+        "#background.breakfast",
+        1,
+        {
+          opacity: 1,
+        },
+        {
+          opacity: 0,
+        }
+      )
+    )
+    .addTo(controller);
+
+  new ScrollMagic.Scene({
+    triggerElement: "#item.lunch",
+    triggerHook: 0,
+  })
+    .setTween(
+      TweenMax.fromTo(
+        "#background.lunch",
+        1,
+        {
+          opacity: 1,
+        },
+        {
+          opacity: 0,
+        }
+      )
+    )
+    .addTo(controller);
+
+  $("#scroll").scrollLeft($("#list #item.breakfast").width() * now());
+
+  $("#control #pre").click(() => control("yesterday"));
+  $("#control #next").click(() => control("tomorrow"));
+  $("#control #date").click(() => control("today"));
+
+  $(document).keydown(function (e) {
+    if (!(e.which >= 37 && e.which <= 40)) return;
     e.preventDefault();
-  }
-  if(e.which == 37) {
-    pre();
-  }
-  if(e.which == 39) {
-    next();
-  }
-  if(e.which == 38) {
-    $('#app #scroll').scrollLeft($('#app #scroll').scrollLeft() - $('#app #scroll').width());
-  }
-  if(e.which == 40) {
-    $('#app #scroll').scrollLeft($('#app #scroll').scrollLeft() + $('#app #scroll').width());
-  }
+    switch (e.which) {
+      case 37:
+        control("yesterday");
+        break;
+      case 39:
+        control("tomorrow");
+        break;
+      case 38:
+        $("#scroll").scrollLeft(
+          $("#scroll").scrollLeft() - $("#scroll").width()
+        );
+        break;
+      case 40:
+        $("#scroll").scrollLeft(
+          $("#scroll").scrollLeft() + $("#scroll").width()
+        );
+        break;
+    }
+  });
+};
+
+$(document).ready(() => {
+  init();
 });
